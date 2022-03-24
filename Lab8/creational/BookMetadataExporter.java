96c861754b9356c32154fffaad6953f2c2f51bd3
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2022-03-24 18:41:02 +0700

    lab8

---
+++ Lab8/creational/BookMetadataExporter.java
@@ -0,0 +1,17 @@
+package creational;
+
+import java.io.PrintStream;
+
+public abstract class BookMetadataExporter extends BookCollection {
+	
+	abstract protected BookMetadataFormatter getFormatter();
+    
+	public void export(PrintStream stream) {
+        // Please implement this method. You may create additional methods as you see fit.
+    	BookMetadataFormatter formatter = this.getFormatter();
+        for (Book book : books) {
+            formatter.append(book);
+        }
+    	stream.println(formatter.getMetadataString());
+    }
+}
